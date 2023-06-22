1107f9fb183e8a394d909dd6f7bf0277ec311a37
Vicho Arhinza <varhinza@gmail.com>
2023-06-22 21:28:17 +0700

    validasi input

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-//Deklarasi global variabel
+// Deklarasi global variabel
 const celsius = document.getElementById("celsius");
 const fahrenheit = document.getElementById("fahrenheit");
 const formula = document.getElementById("formula");
 const explain = document.getElementById("explain");
 let layout = false;
 
-//Fungsi switch konversi suhu 
+// Fungsi switch konversi suhu 
 function reverse() {
     const order = document.getElementById("order");
     order.appendChild(order.firstElementChild); //mengubah urutan input suhu
@@ -13,9 +13,9 @@ function reverse() {
     reset();
 }
 
-//Menampilkan formula
+// Menampilkan formula
 function showFormula(result) {
-    //kondisi untuk menampilkan formula konversi suhu celsius ke fahrenheti atau sebaliknya 
+    // kondisi untuk menampilkan formula konversi suhu celsius ke fahrenheti atau sebaliknya 
     if (!layout) {
         formula.innerHTML = `<b>Formula :</b> ( ${celsius.value} <sup>o</sup>C x 9/5 ) + 32 = ${result} <sup>o</sup>F`;
         explain.innerHTML = `<h3>Konversi <i>S</i><sub>(<sup>o</sup>C)</sub> ke <i>S</i><sub>(<sup>o</sup>F)</sub></h3> 
@@ -31,9 +31,14 @@ function showFormula(result) {
     }
 }
 
-//Fungsi konversi suhu celsius ke fahrenheit
+// Fungsi konversi suhu celsius ke fahrenheit
 function changeE() {
     let result = 9 / 5 * celsius.value + 32; // Formula konversi suhu celsius ke fahrenheit
+    if (isNaN(result)) {// validasi input
+        alert("Masukkan angka");
+        reset();
+        return;
+    }
     /** 
      * membuat kondisi untuk menampilkan konversi suhu celsius ke fahrenheit secara langsung
      * 1. Ketika isi input celsius dihapus
@@ -41,25 +46,30 @@ function changeE() {
      * 3. Ketika user pertama kali input suhu 
      */
     if (!celsius.value && fahrenheit.value) {
-        //hapus isi input fahrenheit, formula & cara konversi
+        // hapus isi input fahrenheit, formula & cara konversi
         fahrenheit.removeAttribute("value");
         fahrenheit.value = '';
         formula.innerHTML = '';
         explain.innerHTML = '';
     } else if (result !== fahrenheit.value) {
-        //ubah nilai fahrenheit dengan hasil konversi & tampilkan formula
+        // ubah nilai fahrenheit dengan hasil konversi & tampilkan formula
         fahrenheit.value = result;
         showFormula(result);
     } else if (celsius.value) {
-        //tambahkan atribut nilai ke input fahrenheit & tampilkan formula
+        // tambahkan atribut nilai ke input fahrenheit & tampilkan formula
         fahrenheit.setAttribute("value", result);
         showFormula(result);
     }
 }
 
-//Fungsi konversi suhu fahrenheit ke celsius
+// Fungsi konversi suhu fahrenheit ke celsius
 function changeF() {
-    let result = (fahrenheit.value - 32) * 5 / 9; //Formula konversi suhu fahrenheit ke celsius
+    let result = (fahrenheit.value - 32) * 5 / 9; // Formula konversi suhu fahrenheit ke celsius
+    if (isNaN(result)) { // validasi input
+        alert("Masukkan angka");
+        reset();
+        return;
+    }
     /** 
      * membuat kondisi untuk menampilkan konversi suhu celsius ke fahrenheit secara langsung
      * 1. Ketika isi input fahrenheit dihapus
@@ -67,17 +77,17 @@ function changeF() {
      * 3. Ketika user pertama kali input suhu 
      */
     if (!fahrenheit.value && celsius.value) {
-        //hapus isi input fahrenheit, formula & cara konversi
+        // hapus isi input fahrenheit, formula & cara konversi
         celsius.removeAttribute("value");
         celsius.value = '';
         formula.innerHTML = '';
         explain.innerHTML = '';
     } else if (result !== celsius.value) {
-        //ubah nilai fahrenheit dengan hasil konversi & tampilkan formula
+        // ubah nilai fahrenheit dengan hasil konversi & tampilkan formula
         celsius.value = result;
         showFormula(result);
     } else if (fahrenheit.value) {
-        //tambahkan atribut nilai ke input fahrenheit & tampilkan formula
+        // tambahkan atribut nilai ke input fahrenheit & tampilkan formula
         celsius.setAttribute("value", result)
         showFormula(result);
     }
@@ -89,7 +99,7 @@ function changeF() {
 celsius.addEventListener('keyup', changeE);
 fahrenheit.addEventListener('keyup', changeF);
 
-//menghapus semua nilai input
+// menghapus semua nilai input
 function reset() {
     celsius.value = '';
     fahrenheit.value = '';
